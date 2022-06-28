ecb302ec2b0bfad3c6c8af5fdf401bc3b69db9b1
Maciej Hołubczat <m.holubczat@perfectgym.com>
2022-06-28 19:24:16 +0200

    work in progress

--- src/main/java/org/example/controller/GameController.java
+++ src/main/java/org/example/controller/GameController.java
@@ -1,13 +1,23 @@
 package org.example.controller;
 
+import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
+import javafx.util.Duration;
+
+import java.net.URL;
+import java.util.ResourceBundle;
 
 public class GameController {
 
+    private boolean right = true;
+    private boolean left = false;
+    private boolean up = false;
+    private boolean down = false;
     @FXML
     public StackPane gamePane;
     @FXML
@@ -16,33 +26,71 @@ public class GameController {
     public Circle head;
 
     void up() {
-        if (head.isVisible()) {
-            System.out.println("UP");
-        }
+        this.up = true;
+        this.down = false;
+        this.left = false;
+        this.right = false;
     }
 
     void down() {
-        if (head.isVisible()) {
-            System.out.println("DOWN");
-        }
+        this.up = false;
+        this.down = true;
+        this.left = false;
+        this.right = false;
     }
 
     void left() {
-        if (head.isVisible()) {
-            System.out.println("LEFT");
-        }
+        this.up = false;
+        this.down = false;
+        this.left = true;
+        this.right = false;
     }
 
     void right() {
-        if (head.isVisible()) {
-            System.out.println("RIGHT");
-        }
+        this.up = false;
+        this.down = false;
+        this.left = false;
+        this.right = true;
     }
 
     void start() {
         System.out.println("START");
         initMsg.setVisible(false);
         head.setVisible(true);
+        move();
+    }
+
+    private void move() {
+        KeyValue goRight = new KeyValue(head.translateXProperty(), 200);
+        KeyValue goUp = new KeyValue(head.translateYProperty(), 200);
+        KeyValue goLeft = new KeyValue(head.translateXProperty(), -200);
+        KeyValue goDown = new KeyValue(head.translateYProperty(), -200);
+        System.out.println("1");
+        while (right && head.isVisible()) {
+            System.out.println("2");
+            new KeyFrame(
+                    Duration.millis(500),
+                    goRight
+            );
+        }
+        while (left && head.isVisible()) {
+            new KeyFrame(
+                    Duration.millis(500),
+                    goLeft
+            );
+        }
+        while (up && head.isVisible()) {
+            new KeyFrame(
+                    Duration.millis(500),
+                    goUp
+            );
+        }
+        while (down && head.isVisible()) {
+            new KeyFrame(
+                    Duration.millis(500),
+                    goDown
+            );
+        }
     }
 }
 
