ba7ed75b590666f6938481954a1acf7833288fcf
mholubczat <m.holubczat@gmail.com>
2022-06-29 17:36:00 +0200

    refector

--- src/main/java/org/example/controller/GameController.java
+++ src/main/java/org/example/controller/GameController.java
@@ -2,95 +2,59 @@ package org.example.controller;
 
 import javafx.animation.KeyFrame;
 import javafx.animation.KeyValue;
+import javafx.animation.Timeline;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
 import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
 import javafx.scene.shape.Circle;
 import javafx.util.Duration;
-
-import java.net.URL;
-import java.util.ResourceBundle;
+import org.example.model.Direction;
 
 public class GameController {
 
-    private boolean right = true;
-    private boolean left = false;
-    private boolean up = false;
-    private boolean down = false;
-    @FXML
-    public StackPane gamePane;
+    Direction direction = Direction.RIGHT;
+    private final Duration STEP = Duration.millis(1500);
+
     @FXML
     public Label initMsg;
     @FXML
     public Circle head;
 
     void up() {
-        this.up = true;
-        this.down = false;
-        this.left = false;
-        this.right = false;
+        if (!direction.equals(Direction.DOWN)) direction = Direction.UP;
     }
 
     void down() {
-        this.up = false;
-        this.down = true;
-        this.left = false;
-        this.right = false;
+        if (!direction.equals(Direction.UP)) direction = Direction.DOWN;
     }
 
     void left() {
-        this.up = false;
-        this.down = false;
-        this.left = true;
-        this.right = false;
+        if (!direction.equals(Direction.RIGHT)) direction = Direction.LEFT;
     }
+
 
     void right() {
-        this.up = false;
-        this.down = false;
-        this.left = false;
-        this.right = true;
+        if (!direction.equals(Direction.LEFT)) direction = Direction.RIGHT;
     }
 
-    void start() {
+    void start(){
         System.out.println("START");
         initMsg.setVisible(false);
         head.setVisible(true);
-        move();
     }
 
+    private KeyValue getDirection() {
+        return switch (direction) {
+            case UP -> new KeyValue(head.translateYProperty(), 10);
+            case DOWN -> new KeyValue(head.translateYProperty(), -10);
+            case LEFT -> new KeyValue(head.translateXProperty(), -10);
+            case RIGHT -> new KeyValue(head.translateXProperty(), 10);
+        };
+    }
+
+
     private void move() {
-        KeyValue goRight = new KeyValue(head.translateXProperty(), 200);
-        KeyValue goUp = new KeyValue(head.translateYProperty(), 200);
-        KeyValue goLeft = new KeyValue(head.translateXProperty(), -200);
-        KeyValue goDown = new KeyValue(head.translateYProperty(), -200);
-        System.out.println("1");
-        while (right && head.isVisible()) {
-            System.out.println("2");
-            new KeyFrame(
-                    Duration.millis(500),
-                    goRight
-            );
-        }
-        while (left && head.isVisible()) {
-            new KeyFrame(
-                    Duration.millis(500),
-                    goLeft
-            );
-        }
-        while (up && head.isVisible()) {
-            new KeyFrame(
-                    Duration.millis(500),
-                    goUp
-            );
-        }
-        while (down && head.isVisible()) {
-            new KeyFrame(
-                    Duration.millis(500),
-                    goDown
-            );
-        }
+        Timeline timeline = new Timeline(new KeyFrame(STEP, getDirection()));
+        timeline.play();
     }
 }
 
