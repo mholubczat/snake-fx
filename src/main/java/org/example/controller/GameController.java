807105caceaa7ffbdeceb789407bec52c8c99e64
mholubczat <m.holubczat@gmail.com>
2022-07-01 13:29:34 +0200

    Add snake body, body movement. Improved this and that. Closes #17

--- src/main/java/org/example/controller/GameController.java
+++ src/main/java/org/example/controller/GameController.java
@@ -10,9 +10,8 @@ import javafx.scene.shape.*;
 import javafx.util.Duration;
 import org.example.model.Direction;
 
-import java.util.HashSet;
-import java.util.LinkedList;
-import java.util.Set;
+import java.util.*;
+import java.util.function.Function;
 
 import static org.example.controller.InitGameController.heigth;
 import static org.example.controller.InitGameController.width;
@@ -24,8 +23,10 @@ public class GameController {
     Direction currentDirection = Direction.RIGHT;
     private final Duration SPEED = Duration.millis(200);
     private final int STEP = 20;
-    private Set<Circle> foods = new HashSet<>();
-    private LinkedList<Circle> snake = new LinkedList<>();
+    private final Set<Circle> FOODS = new HashSet<>();
+    private final LinkedList<Circle> SNAKE = new LinkedList<>();
+    private final Set<Circle> EATEN = new HashSet<>();
+
     @FXML
     public Label initMsg;
     @FXML
@@ -34,54 +35,87 @@ public class GameController {
     void start() {
         initMsg.setVisible(false);
         head.setVisible(true);
-        snake.add(head);
+        SNAKE.add(head);
+        DIRECTIONS.add(currentDirection);
+        System.out.println(SNAKE);
+        System.out.println(SNAKE.getLast());
         addFood();
         move();
     }
 
-    private KeyValue getDirection() {
-        currentDirection = nextDirection;
-        return switch (nextDirection) {
-            case UP -> new KeyValue(head.translateYProperty(), head.getTranslateY() - STEP);
-            case DOWN -> new KeyValue(head.translateYProperty(), head.getTranslateY() + STEP);
-            case LEFT -> new KeyValue(head.translateXProperty(), head.getTranslateX() - STEP);
-            case RIGHT -> new KeyValue(head.translateXProperty(), head.getTranslateX() + STEP);
-        };
+    private final LinkedList<Direction> DIRECTIONS = new LinkedList<>();
+
+    private KeyFrame getKeyFrame(Circle circle, Direction direction) {
+        return new KeyFrame(SPEED, new KeyValue(circle.translateXProperty(), circle.getTranslateX() + STEP * direction.getX()), new KeyValue(circle.translateYProperty(), circle.getTranslateY() + STEP * direction.getY()));
+    }
+
+    private KeyFrame getHeadKeyFrame(Circle circle, Direction direction) {
+        return new KeyFrame(SPEED,
+                e -> {
+                    // consume food
+                    consumeFood();
+                    //Coordinates
+
+                    // next move
+                    move();
+                },
+                new KeyValue(circle.translateXProperty(), circle.getTranslateX() + STEP * direction.getX()), new KeyValue(circle.translateYProperty(), circle.getTranslateY() + STEP * direction.getY()));
     }
 
     private void move() {
         if (Math.random() > 0.95)
             addFood();
-        Timeline timeline = new Timeline(
-                new KeyFrame(SPEED,
-                        e -> {
-                            foods.stream().filter(
-                                    f->f.getBoundsInParent().intersects(head.getBoundsInParent())
-                            ).forEach(f->{
-                                pane.getChildren().remove(f);
-                                if(f.getFill().equals(Color.GREEN)) snake.add(new Circle(5,Color.OLIVE));
-                                else if (snake.size()>1) snake.removeLast();
-                            });
-                            move();
-                        },
-                        getDirection()));
+        currentDirection = nextDirection;
+        DIRECTIONS.addFirst(currentDirection);
+        DIRECTIONS.removeLast();
+        Timeline timeline = new Timeline(getHeadKeyFrame(head, currentDirection));
+        drawBodySegments(timeline);
+        //  timeline.getKeyFrames().add(new KeyFrame(SPEED,))
         timeline.play();
 
+    }
+
+    private void drawBodySegments(Timeline timeline) {
+        for (int i = 1; i < DIRECTIONS.size(); i++) {
+                timeline.getKeyFrames().add(getKeyFrame(SNAKE.get(i), DIRECTIONS.get(i)));
+        }
+    }
+
+
+    private void consumeFood() {
+        FOODS.stream().filter(
+                f -> f.getBoundsInParent().intersects(head.getBoundsInParent())
+        ).forEach(f -> {
+            pane.getChildren().remove(f);
+            EATEN.add(f);
+            if (f.getFill().equals(Color.GREEN)) {
+                Circle newSegment = new Circle(10, Color.OLIVE);
+                newSegment.setTranslateX(SNAKE.getLast().getTranslateX() - STEP * DIRECTIONS.getLast().getX());
+                newSegment.setTranslateY(SNAKE.getLast().getTranslateY() - STEP * DIRECTIONS.getLast().getY());
+                DIRECTIONS.addLast(DIRECTIONS.getLast());
+                SNAKE.add(newSegment);
+                pane.getChildren().add(newSegment);
+            } else if (SNAKE.size() > 1) {
+                pane.getChildren().remove(SNAKE.getLast());
+                SNAKE.removeLast();
+                DIRECTIONS.removeLast();
+            }
+        });
+        FOODS.removeAll(EATEN);
     }
 
     private void addFood() {
         Circle food = new Circle(5);
         food.setTranslateX((int) ((Math.random() - 0.5) * (width - 10)));
         food.setTranslateY((int) ((Math.random() - 0.5) * (heigth - 10)));
-        this.foods.add(food);
-        if (Math.random() > 0.9) {food.setFill(Color.GOLD);}
-        else food.setFill(Color.GREEN);
+        this.FOODS.add(food);
+        if (Math.random() > 0.8) {
+            food.setFill(Color.GOLD);
+        } else food.setFill(Color.GREEN);
         pane.getChildren().add(food);
     }
 
-    private void gainMass(){
 
-    }
 }
 
 
--- src/main/java/org/example/model/Direction.java
+++ src/main/java/org/example/model/Direction.java
@@ -1,5 +1,25 @@
 package org.example.model;
 
 public enum Direction {
-    UP, DOWN, LEFT, RIGHT
+
+    UP (0,-1),
+    DOWN (0,1),
+    LEFT (-1, 0),
+    RIGHT (1, 0);
+
+    private final int X;
+    private final int Y;
+
+    public int getX() {
+        return X;
+    }
+
+    public int getY() {
+        return Y;
+    }
+
+    Direction(int x, int y) {
+        this.X = x;
+        this.Y = y;
+    }
 }
